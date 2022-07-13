ff8a64f2d7ae52332474a8d5062317cda2d1a5e1
Juliya Sartasova <anjee2007@mail.ru>
2022-07-13 17:05:26 +0300

    work with webpack

---
+++ js/modules/cards.js
@@ -0,0 +1,79 @@
+function cards() {
+    // class for Cards
+    class MenuCard {
+        constructor(src, alt, title, description, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            if (this.classes.length === 0 ) {
+                this.classes = 'menu__item';
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.description}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    const getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    };
+
+    getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+        });
+
+    /* пример, как работает get внутри библиотеки
+       async function get(url) {
+            let x = 100;
+            let response = await fetch(url);
+            let ddd = await response.json();
+            return new Promise(function(resolve, reject) {
+                resolve({data2222: ddd}, x);
+       });
+        }*/
+
+    /*    axios.get('http://localhost:3000/menu')
+            .then((data, x) => {
+                console.log(arguments);
+                data.data.forEach(({img, altimg, title, descr, price}) => {
+                    new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+                });
+            });*/
+
+}
+
+module.exports = cards;
